43c6777a0f62fa8611dbff030c1ce4336a882823
Sanju Kamat <sanjukamat123@gmail.com>
2022-05-06 09:46:37 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.09259197357320126,
+        "pitch": -0.0682039043581355,
+        "fov": 1.0566841790587755
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1214469459779224,
+          "pitch": -0.03547960628321434,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.9300527640551177,
+        "pitch": -0.08272261413234361,
+        "fov": 1.1831136236963777
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1009277529802262,
+          "pitch": -0.03170612231131997,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Dining Around Us ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
